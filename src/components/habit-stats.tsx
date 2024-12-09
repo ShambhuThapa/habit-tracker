729f38ff565b87c5d0324e3b69e621ff8ef34754
ShambhuThapa <shambhu.thapa765@gmail.com>
2024-12-09 22:46:38 +0545

    feat: localStoragae implementation

--- src/components/habit-stats.tsx
+++ src/components/habit-stats.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../store/store';
-import { fetchHabits, IHabit } from '../store/habit-slice';
+import React from 'react'
+import {  useSelector } from 'react-redux';
+import {  RootState } from '../store/store';
+import {  IHabit } from '../store/habit-slice';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { LinearProgress } from '@mui/material';
 
 const HabitStats:React.FC = () => {
     const {habits,isLoading,error} = useSelector((state: RootState) => state.habits);
-    const dispatch = useDispatch<AppDispatch>();
+    // const dispatch = useDispatch<AppDispatch>();
 
-    useEffect(() => {
-        dispatch(fetchHabits())
-    }, []);
+    // useEffect(() => {
+    //     dispatch(fetchHabits())
+    // }, []);
 
     const getStreak = (habit: IHabit) => {
         let streak = 0;
@@ -34,6 +34,11 @@ const HabitStats:React.FC = () => {
         return Math.max(...habits.map(habit=>getStreak(habit)),0);
     }
 
+    const getCompletedToday = () => {
+        const today=new Date().toISOString().split("T")[0];
+        return habits.filter(habit => habit.completedDates.includes(today)).length;
+    }
+
     if (isLoading) {
         return <LinearProgress/>;
     }
@@ -45,7 +50,7 @@ const HabitStats:React.FC = () => {
     <Paper elevation={2} sx={{p:2,mt:4}}>
         <Typography variant="h6">Habit Statistics</Typography>
         <Typography variant="body1">Total Habits: {habits.length}</Typography>
-        <Typography variant="body1">Completed Today: {habits.length}</Typography>
+        <Typography variant="body1">Completed Today: {getCompletedToday()}</Typography>
         <Typography variant="body1">Longest Streak: {getLongestStreak()}</Typography>
     </Paper>
   )
--- src/store/habit-slice.ts
+++ src/store/habit-slice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 export interface IHabit {
     id: string,
     name: string;
@@ -9,41 +9,53 @@ export interface IHabit {
 
 interface HabitState {
     habits: IHabit[],
-    isLoading:boolean,
-    error:string | null
+    isLoading: boolean,
+    error: string | null
 }
 
-const initialState: HabitState = {
-    habits: [],
-    isLoading: false,
-    error: null
+
+const fetchFromLocalStorage = () => {
+    const data = localStorage.getItem('habits');
+    if (data) {
+        return JSON.parse(data)
+    } else {
+        return [];
+    }
 }
 
-export const fetchHabits = createAsyncThunk('habits/fetchHabits', async () => {
-    //Simulating an API call
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    const mockHabits:IHabit[] = [
-        {
-            id: '1',
-            name: "Read a book",
-            frequency: "daily",
-            completedDates: [],
-            createdAt: new Date().toISOString(),
-        },
-        {
-            id: '2',
-            name: "Exercise",
-            frequency: "daily",
-            completedDates: [],
-            createdAt: new Date().toISOString(),
-        }
-    ]
-    return mockHabits;
-})
+const storeInLocalStorage = (data: IHabit[]) => {
+    localStorage.setItem('habits', JSON.stringify(data))
+}
+
+// export const fetchHabits = createAsyncThunk('habits/fetchHabits', async () => {
+//     //Simulating an API call
+//     await new Promise(resolve => setTimeout(resolve, 1000));
+//     const mockHabits: IHabit[] = [
+//         {
+//             id: '1',
+//             name: "Read a book",
+//             frequency: "daily",
+//             completedDates: [],
+//             createdAt: new Date().toISOString(),
+//         },
+//         {
+//             id: '2',
+//             name: "Exercise",
+//             frequency: "daily",
+//             completedDates: [],
+//             createdAt: new Date().toISOString(),
+//         }
+//     ]
+//     return mockHabits;
+// })
 
 const habitSilce = createSlice({
     name: "habits",
-    initialState,
+    initialState: {
+        habits: fetchFromLocalStorage(),
+        isLoading: false,
+        error: null,
+    } as HabitState,
     reducers: {
         addHabit: (
             state,
@@ -57,9 +69,10 @@ const habitSilce = createSlice({
                 createdAt: new Date().toISOString(),
             }
             state.habits.push(newHabit);
+            storeInLocalStorage(state.habits);
         },
         toggleHabit: (state, action: PayloadAction<{ id: string, date: string }>) => {
-            const habit = state.habits.find(item => item.id === action.payload.id);
+            const habit = state.habits.find((item: IHabit) => item.id === action.payload.id);
             if (habit) {
                 const index = habit.completedDates.indexOf(action.payload.date);
                 if (index > -1) {
@@ -67,25 +80,27 @@ const habitSilce = createSlice({
                 } else {
                     habit.completedDates.push(action.payload.date)
                 }
+                storeInLocalStorage(state.habits);
             }
         },
         deleteHabit: (state, action: PayloadAction<{ id: string }>) => {
-            state.habits = state.habits.filter(item => item.id !== action.payload.id);
+            state.habits = state.habits.filter((item: IHabit) => item.id !== action.payload.id);
+            storeInLocalStorage(state.habits);
         }
-    },
-    extraReducers: (builder) => {
-        builder.addCase(fetchHabits.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(fetchHabits.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.habits = action.payload;
-        })
-        .addCase(fetchHabits.rejected, (state, action) => {
-            state.isLoading = false;
-            state.error = action.error.message || "Failed to fetch habits";
-        })
     }
+    // extraReducers: (builder) => {
+    //     builder.addCase(fetchHabits.pending, (state) => {
+    //         state.isLoading = true;
+    //     })
+    //         .addCase(fetchHabits.fulfilled, (state, action) => {
+    //             state.isLoading = false;
+    //             state.habits = action.payload;
+    //         })
+    //         .addCase(fetchHabits.rejected, (state, action) => {
+    //             state.isLoading = false;
+    //             state.error = action.error.message || "Failed to fetch habits";
+    //         })
+    // }
 })
 
 export const { addHabit, toggleHabit, deleteHabit } = habitSilce.actions
